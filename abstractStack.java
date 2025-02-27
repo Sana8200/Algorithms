0b8a43b3a2a6a330b335f04c2cf1e97112cec9b2
Sana <144557568+Sana8200@users.noreply.github.com>
2025-02-27 03:40:27 +0100

    Update abstractStack.java

--- abstractStack.java
+++ abstractStack.java
@@ -1,1 +1,16 @@
 
+public abstract class abstractStack {
+    int stack[];
+    int size;
+    int top;
+
+    public abstractStack(int size) {
+        this.size = size;
+        this.stack = new int[size];
+        this.top = 0;
+    }
+
+    public abstract void push(int value);
+
+    public abstract int pop();
+}
