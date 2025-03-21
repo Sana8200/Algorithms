6134e7c2ebae2d3f81d401ba966655bc079d7c88
Sana <144557568+Sana8200@users.noreply.github.com>
2025-03-21 13:07:36 +0100

    Create Queue.java

---
+++ Queues/Queue.java
@@ -0,0 +1,18 @@
+/*
+   FIFO   first in first out
+   if you add an item to the queue you will have to wait for all other items
+   in the queue to be removed before your new item is in the front of teh queue
+
+   Queues have their own way of doing things, A queue is an interface and not a class,
+    is more like a template that we can apply to another class
+   A collection designed for holding elements prior to processing
+   Linear data structure
+
+   add = enqueue, offer()
+   remove = dequeue, poll()
+ */
+
+
+public class Queue {
+
+}
